860b0f146f68ed92e844ef1dd29fb98c127a566f
Navpreet Kaur <navpreet111997@gmail.com>
2018-12-14 14:34:02 -0500

    first commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Xcode User on 2018-10-06.
+//  Copyright © 2018 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func unwindToThisViewController(segue: UIStoryboardSegue)
+    {
+        
+    }
+}
+
